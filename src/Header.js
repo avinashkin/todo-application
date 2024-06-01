7c21ede22aa2513d1eff2b4aad88f53ba5032c2c
Avinash Gupta <guptaavinash92@gmail.com>
2024-06-01 16:20:29 +0100

    Adding icons

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,13 @@
+import bell from "../src/images/bell.png";
+import user from "../src/images/user.png";
 const Header = () => {
     return (
-        <header className="w-full h-16 shadow-lg flex items-center">
+        <header className="w-full h-16 shadow-lg flex items-center justify-between">
             <h1 className="pl-28 align-middle text-3xl font-black">Todo List</h1>
+            <div className="pr-20 gap-8 flex">
+                <img src={bell} className="cursor-pointer" alt="notification" width="30" height="20" />
+                <img src={user} className="cursor-pointer" alt="user" width="30" height="20" />
+            </div>
         </header>
     );
 };
